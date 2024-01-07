3f0242be91846763cbd37ee28547a18ae4bf5e78
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-01-07 15:48:54 +0100

    inizio

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const numerogiocatoriinput = document.getElementById("numeroGiocatori");
+const numerogiocatoributton = document.getElementById("confermanumero");
+const divmain = document.getElementById("main");
+const divgiocatori = document.getElementById("giocatori");
+
+numerogiocatoributton.addEventListener("click", ()=>{
+console.log("ciao");
+});
