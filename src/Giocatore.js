0dabafcaec5682ee2859d0b7b00d378f6c090c6c
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-01-10 23:04:10 +0100

    inizio finestre di dettaglio - utente e cella

---
+++ src/Giocatore.js
@@ -0,0 +1,32 @@
+export function Giocatore(nome, posizione = 0) {
+    this.nome = nome;
+    this.posizione = posizione;
+    this.denaro = 1000;
+    this.lanciaDado = function () {
+      return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    };
+    this.show = () => {
+      return "<div class='giocatore col-auto'>" + nome.charAt(0).toUpperCase() + "</div>";
+    };
+    this.cambiaPosizione = function (boolean) {
+      if (boolean) {
+        this.posizione += this.lanciaDado() + this.lanciaDado();
+        console.log("start: "+this.posizione);
+        if(this.posizione > 40){
+            this.posizione = this.posizione - 40;
+            console.log("Passaggio dal via");
+            this.denaro += 200;
+        }
+        console.log("Posizione: "+this.posizione);
+        return this.posizione;
+      } else {
+        console.log("Non è il tuo turno");
+      }
+    };
+    this.getPosizione = function(){
+        return this.posizione
+    }
+    this.getDenaro = function(){
+        return this.denaro;
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
+import { Giocatore } from "./Giocatore.js";
 const tiraDadiButton = document.getElementById("lancio");
-
-function Giocatore(nome, posizione = 0) {
-    this.nome = nome;
-    this.posizione = posizione;
-    this.lanciaDado = function () {
-      return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-    };
-    this.show = () => {
-      return "<div class='giocatore col-auto'>" + nome.charAt(0).toUpperCase() + "</div>";
-    };
-    this.cambiaPosizione = function (boolean) {
-      if (boolean) {
-        this.posizione += this.lanciaDado() + this.lanciaDado();
-        console.log("start: "+this.posizione);
-        if(this.posizione > 40)
-            this.posizione = this.posizione - 40;
-            console.log("Posizione: "+this.posizione);
-        return this.posizione;
-      } else {
-        console.log("Non è il tuo turno");
-      }
-    };
-    this.getPosizione = function(){
-        return this.posizione
-    }
-  }
-  
-
+const paginaDettaglioGiocatore = document.getElementById("paginaDettaglioGiocatore");
+const paginaDettaglioCasella = document.getElementById("paginaDettaglioCasella");
 window.onload = () =>{
     for(let i=0;i<40;i++){
             const div = document.getElementById("cell"+i);
             div.addEventListener("click", () =>{
+                paginaDettaglioCasella.innerHTML = "<p>ID: "+div.id+"</p>";
             console.log("click: "+div.id);
         })
     }
@@ -38,6 +14,6 @@ window.onload = () =>{
     document.getElementById("intDiv0").innerHTML = persona.show(0);
     tiraDadiButton.onclick = () =>{
         persona.cambiaPosizione(true);
-        document.getElementById("intDiv"+persona.getPosizione()).innerHTML = persona.show();
+        document.getElementById("intDiv" + persona.getPosizione() ).innerHTML = persona.show();
     }
 };
