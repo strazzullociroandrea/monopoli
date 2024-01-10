703f6045ec043a63e2608b8fa62b82d6e2a2438c
ciroandrea <strazzullociroandrea@itis-molinari.eu>
2024-01-10 22:49:27 +0100

    inizio movimento singolo giocatore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const tiraDadiButton = document.getElementById("lancio");
+
+function Giocatore(nome, posizione = 0) {
+    this.nome = nome;
+    this.posizione = posizione;
+    this.lanciaDado = function () {
+      return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    };
+    this.show = () => {
+      return "<div class='giocatore col-auto'>" + nome.charAt(0).toUpperCase() + "</div>";
+    };
+    this.cambiaPosizione = function (boolean) {
+      if (boolean) {
+        this.posizione += this.lanciaDado() + this.lanciaDado();
+        console.log("start: "+this.posizione);
+        if(this.posizione > 40)
+            this.posizione = this.posizione - 40;
+            console.log("Posizione: "+this.posizione);
+        return this.posizione;
+      } else {
+        console.log("Non è il tuo turno");
+      }
+    };
+    this.getPosizione = function(){
+        return this.posizione
+    }
+  }
+  
+
 window.onload = () =>{
     for(let i=0;i<40;i++){
             const div = document.getElementById("cell"+i);
@@ -5,7 +34,10 @@ window.onload = () =>{
             console.log("click: "+div.id);
         })
     }
-    document.getElementById("intDiv0").innerHTML = "<div class='giocatore col-auto'>g1</div><div class='giocatore col-auto'>g2</div><div class='giocatore col-auto'>g3</div><div class='giocatore col-auto'>g4</div>";
+    const persona = new Giocatore("Ciro");
+    document.getElementById("intDiv0").innerHTML = persona.show(0);
+    tiraDadiButton.onclick = () =>{
+        persona.cambiaPosizione(true);
+        document.getElementById("intDiv"+persona.getPosizione()).innerHTML = persona.show();
+    }
 };
-
-
