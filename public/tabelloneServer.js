d736a64e80adf8a9a30df7afcdce35bf9e56c337
CiroAS <ciroandreastrazzullo06@gmail.com>
2024-07-02 20:31:50 +0200

    Socket server

--- public/tabelloneServer.js
+++ public/tabelloneServer.js
@@ -14,28 +14,28 @@ const getPartecipanti = async() =>{
         })
     });
     rsp = await rsp.json();
+    partecipanti.innerHTML = "";
+    rsp.result.forEach(partecipante=>{
+        partecipanti.innerHTML += `
+        <div style="display: flex; align-items: center; margin-bottom: 5px;">
+            <div style="width: 20px; height: 20px; background-color: %COLOR; margin-right: 10px;"></div>
+            <span>%NOME</span>
+        </div>
+    `.replace("%NOME", partecipante.nome)
+     .replace("%COLOR", partecipante.pedina)
+    })
 }
 
 window.onload = () =>{
     titolo.value = sessionStorage.getItem("titolo");
     codice.value = sessionStorage.getItem("codice");
     socket.emit("aggiungiserver", sessionStorage.getItem("codice"));
-    getPartecipanti();
 }
 
 socket.on("aggiuntoserver",(response)=>{
-    console.log("Server aggiunto? "+response);
+    getPartecipanti();
 })
 
 socket.on("aggiuntopartecipante", (partecipanteNuovo)=>{
-    console.log("Partecipante XYZ aggiunto alla partita");
-    console.log(JSON.parse(partecipanteNuovo));
-    /*partecipanti.innerHTML += `
-        <div style="display: flex; align-items: center; margin-bottom: 5px;">
-            <div style="width: 20px; height: 20px; background-color: %COLOR; margin-right: 10px;"></div>
-            <span>%NOME</span>
-        </div>
-    `.replace("%NOME", partecipanteNuovo.nome)
-     .replace("%COLOR", partecipanteNuovo.pedina)
-    */
+    window.onload();
 })
